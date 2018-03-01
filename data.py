4add779eebd7655a33641819d28af9023fdbb425
JanoHorvath <jano.horvath@gmail.com>
2018-03-01 18:26:03 +0000

    Added input_reading function & defined Map, Vehicle and Ride class

--- data.py
+++ data.py
@@ -1,13 +1,49 @@
+class Map(object):
+    def __init__(self, rows, cols, no_vehicles, no_rides, bonus, T):
+        self.rows = rows
+        self.cols = cols
+        self.bonus = bonus
+        self.T = T
 
 
-'''
-returns input data dictionary
-'''
+class Vehicle(object):
+    def __init__(self):
+        self.cur_pos = [0, 0]
+
+
+class Ride(object):
+    def __init__(self, start_x, start_y, end_x, end_y, t_start, t_finish):
+        self.start_loc = [start_x, start_y]
+        self.end_loc = [end_x, end_y]
+        self.start_t = t_start
+        self.end_t = t_finish
+        self.duration = abs(end_x - start_x) + abs(end_y - start_y)
+        self.start_t_max = self.end_t - self.duration
+
+
 def load(infile):
-  pass
+    ''' Parses infile.
 
-'''
-accepts output file and output data dictionary
-'''
+    Returns:
+    m - Map object
+    vehicles - list of Vehicle objects
+    rides - list of Ride objects
+    '''
+    with open(infile) as f:
+        first_line = map(int, f.readline().split())
+        m = Map(*first_line)
+        vehicles = []
+        for _ in range(first_line[2]):
+            vehicles.append(Vehicle())
+
+        rides = []
+        for ride_line in f.readlines():
+            rides.append(Ride(*map(int, ride_line.split())))
+
+    return m, vehicles, rides
+
+
 def store(outfile, data):
-  pass
+    ''' Accepts output file and output data dictionary
+    '''
+    pass
--- run.py
+++ run.py
@@ -1,26 +1,23 @@
 #!/bin/env python
 
+import sys
+
 from data import load, store
 from proc import process
 
-import sys
-
 if __name__ == '__main__':
-  if len(sys.argv) != 3:
-    print "usage: ./run.py <inputfile> <outputfile>"
-    sys.exit(0)
+    if len(sys.argv) != 3:
+        print "usage: ./run.py <inputfile> <outputfile>"
+        sys.exit(0)
 
-  infile = sys.argv[1]
-  outfile = sys.argv[2]
+    infile = sys.argv[1]
+    outfile = sys.argv[2]
 
-  print 'loading input from', infile
-  data = load(infile)
+    print 'loading input from', infile
+    data = load(infile)
 
-  print 'processing ...'
-  data = process(data)
+    print 'processing ...'
+    data = process(data)
 
-  print 'storing output to', outfile
-  store(outfile, data)
-
-
-
+    print 'storing output to', outfile
+    store(outfile, data)
